6f4cd607629979c148039f36553155550acf4bf6
DemeoStep <demeo.alex@gmail.com>
2021-08-28 13:33:27 +0300

    MASTER: Task2 completed

--- src/homework1/Main.java
+++ src/homework1/Main.java
@@ -4,13 +4,26 @@ import homework1.add_task.Address;
 import homework1.task2.Rectangle;
 import homework1.task4.Computer;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         /*----------ADDITIONAL TASK---------*/
         Address address = new Address();
 
         /*----------TASK 2---------*/
-        Rectangle rectangle = new Rectangle();
+        System.out.println("/*----------TASK 2---------*/");
+
+        System.out.print("Введите длину прямоугольника: ");
+        double side1 = input.nextDouble();
+        System.out.print("Введите ширину прямоугольника: ");
+        double side2 = input.nextDouble();
+
+        Rectangle rectangle = new Rectangle(side1, side2);
+        rectangle.printRectangle();
+
+        System.out.println("/*----------TASK 2---------*/\n\n");
 
         /*----------TASK 3---------*/
         Computer computer = new Computer();
--- src/homework1/task2/Rectangle.java
+++ src/homework1/task2/Rectangle.java
@@ -1,4 +1,16 @@
 package homework1.task2;
 
 public class Rectangle {
+    final private double side1;
+    final private double side2;
+
+    public Rectangle(double side1, double side2) {
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+
+    public void printRectangle() {
+        System.out.println("Площадь: " + this.side1 * this.side2);
+        System.out.println("Периметр: " + ((this.side1 + this.side2) * 2));
+    }
 }
