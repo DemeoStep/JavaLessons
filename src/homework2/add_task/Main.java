49c9175ba3b1aa2b876ea2bb4c542d007408501b
DemeoStep <demeo.alex@gmail.com>
2021-09-04 13:00:34 +0300

    MASTER: add_task from homework2 complete

--- src/homework2/add_task/Main.java
+++ src/homework2/add_task/Main.java
@@ -1,7 +1,13 @@
 package homework2.add_task;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Введите радиус: ");
+        Scanner in = new Scanner(System.in);
+        double radius = in.nextDouble();
 
+        MyArea.areaOfCircle(radius);
     }
 }
--- src/homework2/add_task/MyArea.java
+++ src/homework2/add_task/MyArea.java
@@ -1,4 +1,10 @@
 package homework2.add_task;
 
 public class MyArea {
+    final static double PI = 3.14;
+
+    public static void areaOfCircle (double radius) {
+        double square = PI * Math.pow(radius, 2);
+        System.out.println("Площадь круга: " + square);
+    }
 }
